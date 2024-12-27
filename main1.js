e9edd05e42af886eea5f306a695e28e3d8d6ddaa
mii24 <189688899+mii24@users.noreply.github.com>
2024-12-27 11:12:18 +0900

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,14 +1,14 @@
 
 //使用する変数を宣言
-var startButton;    // startボタン
-var stopButton;     // stopボタン
-var resetButton;    // resetボタン
-var showTime;       // 表示時間
+let startButton;    // startボタン
+let stopButton;     // stopボタン
+let resetButton;    // resetボタン
+let showTime;       // 表示時間
 
-var timer;              // setinterval, clearTimeoutで使用
-var startTime;          // 開始時間
-var elapsedTime = 0;    // 経過時間
-var holdTime = 0;       // 一時停止用に時間を保持
+let timer;              // setinterval, clearTimeoutで使用
+let startTime;          // 開始時間
+let elapsedTime = 0;    // 経過時間
+let holdTime = 0;       // 一時停止用に時間を保持
 
 
 //ページ読み込み後、変数にHTML要素を設定。ボタンや計測時間の表示のidを指定して各変数にHTML要素を設定
@@ -70,23 +70,23 @@ function measureTime() {
         elapsedTime = Date.now() - startTime + holdTime;
 
         //1時間 = 3600秒 = 3600 * 1000ミリ秒
-        var hours = Math.floor(elapsedTime / 3600000);
+        let hours = Math.floor(elapsedTime / 3600000);
 
         //経過時間を3600000で割った余りを計算。これにより、1時間未満の経過時間が得られる
         //その結果を60000（1分 = 60秒 = 60 * 1000ミリ秒）で割ることで、経過時間を分単位に変換
         //Math.floorを使って小数点以下を切り捨て、整数部分だけを取得
-        var minutes = Math.floor((elapsedTime % 3600000) / 60000);
+        let minutes = Math.floor((elapsedTime % 3600000) / 60000);
 
         //経過時間を60000で割った余りを計算。これにより、1分未満の経過時間が得られる
         // その結果を1000（1秒 = 1000ミリ秒）で割ることで、経過時間をる秒単位に変換
         // Math.floorを使って小数点以下を切り捨て、整数部分だけを取得
-        var seconds = Math.floor((elapsedTime % 60000) / 1000);
+        let seconds = Math.floor((elapsedTime % 60000) / 1000);
 
         //経過時間を1000で割った余りを計算します。これにより、1秒未満の経過時間が得られる
         // その結果を100で割ることで、ミリ秒を1桁に変換
         // （例えば、123ミリ秒なら1.23となり、Math.floorで1）
         // Math.floorを使って小数点以下を切り捨て、整数部分だけを取得
-        var milliseconds = Math.floor((elapsedTime % 1000) / 100); // ミリ秒を1桁にする
+        let milliseconds = Math.floor((elapsedTime % 1000) / 100); // ミリ秒を1桁にする
         showTime.textContent = hours + ":" + minutes + ":" + seconds + ":" + milliseconds;
 
       // 関数を呼び出し、時間計測を継続する
